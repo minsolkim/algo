7f813b7e150e856dcedd63e2c4b485a916c63821
kimminsol <123alsthf@naver.com>
2023-06-10 22:39:34 +0900

    과일장수

---
+++ src/lv1/day9/과일장수.java
@@ -0,0 +1,23 @@
+package lv1.day9;
+
+import java.util.Arrays;
+
+public class 과일장수 {
+    public static int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Arrays.sort(score); 
+        for(int i=score.length;i >= m ;i-=m){
+            answer += score[i-m] * m;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int k = 4;
+        int m = 3;
+        int[] score = {4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2};
+
+        System.out.println(solution(k, m, score));
+    }
+}
