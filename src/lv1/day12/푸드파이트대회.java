ea8d877b54af984b1e3656194817796069165925
kimminsol <123alsthf@naver.com>
2023-06-13 17:02:21 +0900

    푸드파이트대회

---
+++ src/lv1/day12/푸드파이트대회.java
@@ -0,0 +1,30 @@
+package lv1.day12;
+
+import java.util.ArrayList;
+
+public class 푸드파이트대회 {
+    public static String solution(int[] food) {
+        String answer = "";
+        for(int i=0; i<food.length;i++){
+            if(food[i] ==1)
+                continue;
+            for(int j=0;j<food[i]/2;j++){
+                answer +=i;
+            }
+        }
+        answer+="0";
+        for(int i=food.length-1;i>=1;i--){
+            if(food[i] ==1)
+                continue;
+            for(int j =0;j< food[i]/2;j++){
+                answer+=i;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] food ={1,3,4,6};
+        System.out.print(solution(food));
+    }
+}
