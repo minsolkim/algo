0eee093c95ba4559698248637808b2f916dd0387
kimminsol <123alsthf@naver.com>
2023-08-23 20:36:51 +0900

    성격유형검사하기

--- src/lv1/성격유형검사하기.java
+++ src/lv1/성격유형검사하기.java
@@ -3,18 +3,19 @@ package lv1;
 import java.util.HashMap;
 
 public class 성격유형검사하기 {
-    static String[] type = {"RT","CF","JM","AN"};
+    static String[] types = {"RT","CF","JM","AN"};
     public static String typeCheck(HashMap<Character,Integer> map) {
         StringBuilder sb = new StringBuilder();
-        char firstchar;
-        char secondchar;
-        char typechar;
-        for(String s : type){
-            firstchar = s.charAt(0);
-            secondchar = s.charAt(1);
-
-            typechar = map.get(firstchar) >= map.get(secondchar) ? firstchar : secondchar;
-            sb.append(typechar);
+        char leftvalue;
+        char rightvalue;
+        for(String s : types){
+            leftvalue = s.charAt(0);
+            rightvalue = s.charAt(1);
+            if(map.get(leftvalue) >= map.get(rightvalue)) {
+                sb.append(leftvalue);
+            }else if(map.get(leftvalue) < map.get(rightvalue)) {
+                sb.append(rightvalue);
+            }
         }
         return sb.toString();
     }
@@ -22,15 +23,18 @@ public class 성격유형검사하기 {
         int []score = {0,3,2,1,0,-1,-2,-3};
         int scoreSum = 0;
         HashMap<Character, Integer> map = new HashMap<>();
-        for (String s : type){
-            map.put(s.charAt(0),0);
-            map.put(s.charAt(1),0);
+        //점수기록 기본값으로 초기화
+        for (String type : types){
+            map.put(type.charAt(0),0);
+            map.put(type.charAt(1),0);
         }
-        for(int i=0;i<survey.length;i++) {
+        //설문을 돌린다.
+        int n = survey.length;
+        for(int i=0;i<n;i++) {
+            //기본값에 점수를 더해준다.
             scoreSum = map.get(survey[i].charAt(0))+score[choices[i]];
             map.put(survey[i].charAt(0),scoreSum);
         }
-
 
         String answer =typeCheck(map);
         return answer;
