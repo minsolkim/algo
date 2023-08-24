6bef44563e0d9711b52a10e1420009a28e322668
kimminsol <123alsthf@naver.com>
2023-08-24 20:19:39 +0900

    문자열내림차순으로배치하기

---
+++ src/lv1/day20/문자열내림차순으로배치하기.java
@@ -0,0 +1,12 @@
+package lv1.day20;
+import java.util.Arrays;
+public class 문자열내림차순으로배치하기 {
+        public String solution(String s) {
+            char[] str = s.toCharArray();
+            Arrays.sort(str);
+            StringBuffer sb = new StringBuffer(new String(str));
+
+            String answer = sb.reverse().toString();
+            return answer;
+        }
+}
