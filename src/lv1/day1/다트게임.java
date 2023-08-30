02cccb2913c22b0020a70d43b495454ae1c7a914
kimminsol <123alsthf@naver.com>
2023-08-30 23:01:22 +0900

    다트게임

--- src/lv1/day1/다트게임.java
+++ src/lv1/day1/다트게임.java
@@ -1,48 +1,51 @@
 package lv1.day1;
 
-import java.util.ArrayList;
-import java.util.Stack;
 
 public class 다트게임 {
     public int solution(String dartResult) {
-        int answer = 0;
-        Stack<Integer> result = new Stack<>();
-        int pointer = 0;
-
+        int answer =0;
+        int[] result = new int[3];
+        char[] dart = dartResult.toCharArray();
+        int sum = 0;
+        int dartCnt = 0;
+        //하나씩 쪼갠다
         for (int i = 0; i < dartResult.length(); i++) {
-            char dart = dartResult.charAt(i);
-            //숫자일경우 바로 수행문을 수행하지 않고 다시 조건문으로 넘어감
-            if(Character.isDigit(dart))
-                continue;
-
-            String str = dartResult.substring(pointer, i);
-            int num = !str.isBlank()?Integer.parseInt(str):0;
-            switch (dart) {
-                case 'S':
-                    break;
-                case 'D':
-                    num = num*num;
-                    break;
-                case 'T':
-                    num = num*num*num;
-                    break;
-                case '*':
-                    num = result.pop()*2;
-                    if(!result.isEmpty())result.push(result.pop()*2);
-                    break;
-                case '#':
-                    num = result.pop()*-1;
-                    break;
+            //숫자일 경우
+            if (Character.isDigit(dart[i])) {
+                if (i != 0) {
+                    result[dartCnt++] = sum;
+                }
+                //10일 경우 예외
+                if ((int) dart[i + 1] == '0') {
+                    sum = 10;
+                    i++;
+                    continue;
+                } else sum = dart[i] - '0';
             }
-            pointer=i+1;
-            result.push(num);
-
+            if (dart[i] == 'S') {
+                ;
+            } else if (dart[i] == 'D') {
+                sum *= sum;
+            } else if (dart[i] == 'T') {
+                sum = (int) Math.pow(sum, 3);
+            } else if (dart[i] == '*') {
+                sum *= 2;
+                //바로앞에꺼 2배
+                if (dartCnt!=0) {
+                    result[dartCnt-1] *=2;
+                }
+            } else if (dart[i] == '#') {
+                sum *= -1;
+            }
+            if (i == dartResult.length() - 1 && dartCnt == 2) {
+                result[dartCnt] = sum;
+            }
         }
-        while(!result.isEmpty())
-            answer+=result.pop();
-
-        return answer;
-    }
+            for (int i: result) {
+                answer +=i;
+            }
+            return answer;
+        }
     public static void main(String[] args) {
         다트게임 test = new 다트게임();
         System.out.println(test.solution("1S2D*3T"));
