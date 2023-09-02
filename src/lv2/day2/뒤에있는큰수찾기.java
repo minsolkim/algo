7e9e2a5faa83e340fc7d8a9c2c62845c35b205e5
kimminsol <123alsthf@naver.com>
2023-09-02 15:52:09 +0900

    뒤에있는큰수찾기

---
+++ src/lv2/day2/뒤에있는큰수찾기.java
@@ -0,0 +1,34 @@
+package lv2.day2;
+
+import java.util.Stack;
+
+public class 뒤에있는큰수찾기 {
+    public static int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i= numbers.length-1;i>=0;i--) {
+            while (!stack.isEmpty()) {
+                int num = stack.peek();
+                //뒤에있는 수가 크다면
+                if (num>numbers[i]){
+                    answer[i] = num;
+                    break;
+                }else {
+                    stack.pop();
+                }
+            }
+            if (stack.isEmpty()){
+                answer[i] =-1;
+            }
+
+            stack.push(numbers[i]);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2,3,3,5};
+        System.out.print(solution(numbers));
+    }
+}
