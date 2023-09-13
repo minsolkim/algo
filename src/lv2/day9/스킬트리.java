b4af4ed0356a5ab48639acc5441283582c9c55df
kimminsol <123alsthf@naver.com>
2023-09-13 20:30:03 +0900

    스킬트리

--- src/lv2/day9/스킬트리.java
+++ src/lv2/day9/스킬트리.java
@@ -1,35 +1,49 @@
 package lv2.day9;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class 스킬트리 {
     public static int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        String[] ad = skill.split("");
-        for (int i=0;i<skill_trees.length;i++) {
-            if(checkskill(ad,skill_trees[i]))
-                answer++;
+        HashMap<Character,Integer> map = new HashMap<>();
+        //선행스킬이 적용안되는 일반스킬만 있을 경우 무조건 true
+        boolean check = true;
+        for (int i=0;i<skill.length();i++){
+            //(선행스킬,순서) 저장
+            map.put(skill.charAt(i),i);
         }
-        return answer;
-    }
-    static boolean checkskill(String[] skills, String skillorder) {
-        int cnt =0;
-        for (int i=0;i<skillorder.length();i++) {
-            for (int j = cnt; j < skills.length;j++) {
-                if (skillorder.charAt(i) == skills[j].charAt(0)){
-                    if (cnt < j) {
-                        return false;
-                    }
-                    cnt++;
+        char[] skills;
+        for (int i=0;i< skill_trees.length;i++){
+            skills = skill_trees[i].toCharArray();
+            //순서대로 스킬이 들어가있으므로 초기값을 0으로 함
+            int skillorder=0;
+            for (int j=0;j<skills.length;j++) {
+                //선행스킬순서 알파벳인지
+                if (!map.containsKey(skills[j]))
+                    continue;
+                //선행스킬 이후로 후행스킬 적용
+                if (map.get(skills[j]) == skillorder) {
+                    //선행스킬이 있는경우 그다음 스킬 적용 가능 
+                    skillorder++;
+                }else {
+                    //선행스킬 전에 후행수킬을 적용할 경우
+                    check = false;
+                    break;
                 }
             }
+            if (check) {
+                //선행스킬이 순서에 맞게 적용된경우
+                answer++;
+            }else {
+                check = true;
+            }
         }
-        return true;
+        return answer;
     }
 
     public static void main(String[] args) {
         String skill = "BAC";
-        String[] skill_trees = { "BACDE", "CBADF", "AECB", "BDAC", "TCEA" };
+        String[] skill_trees = { "DE", "CBADF", "AECB", "BDAC", "TCEA" };
         System.out.println(solution(skill,skill_trees));
 
     }
---
+++ src/lv2/다음큰숫자.java
@@ -0,0 +1,8 @@
+package lv2;
+
+public class 다음큰숫자 {
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
